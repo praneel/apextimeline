fb8035d74c2feb020a24e86ac165df421bc8a8d3
Anand BN <anarasimhan@salesforce.com>
2013-11-02 20:59:11 -0400

    Included JSON data in a separate tab.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,11 +4,14 @@ import java.io.File;
 import java.util.List;
 
 import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import com.anand.salesforce.log.utils.SFDCLogParser;
 import com.anand.salesforce.log.operations.Operation;
 import play.libs.Json;
+
 import play.mvc.*;
+import scala.util.parsing.json.JSON;
 
 import views.html.*;
 
@@ -22,6 +25,7 @@ public class Application extends Controller {
 		return ok(sample.render());
 	}
 
+	@SuppressWarnings("deprecation")
 	public static Result showTimeline() throws Exception {
 		Http.MultipartFormData body = request().body().asMultipartFormData();
 		Http.MultipartFormData.FilePart resourceFile = body.getFile("logFile");
@@ -32,8 +36,12 @@ public class Application extends Controller {
 			SFDCLogParser parser = new SFDCLogParser();
 			Operation top = parser.parseLogFile(file,100);
 			List<Operation> oprList = parser.getFlattenedDataForUI(top);
+			ObjectMapper mapper =new ObjectMapper();
 			JsonNode json = Json.toJson(oprList);
-			return ok(showTimeLine.render(json));
+			System.out.println();
+			return ok(showTimeLine.render(json,
+										  mapper.defaultPrettyPrintingWriter().writeValueAsString(json))
+					);
 		}else{
 			return badRequest();
 		}
